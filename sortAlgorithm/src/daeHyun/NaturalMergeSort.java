4b9e6257da21f1e8630cd0ce2d69ac7328f17612
daehyun <kimdeahyeon5445@gmail.com>
2022-05-26 00:44:27 +0900

    NaturalMergeSort

--- sortAlgorithm/src/daeHyun/NaturalMergeSort.java
+++ sortAlgorithm/src/daeHyun/NaturalMergeSort.java
@@ -4,28 +4,20 @@ public class NaturalMergeSort {
 
     public static void sort(Comparable[] arr) {
         Comparable[] tempArr = new Comparable[arr.length];
-        int idx = 0;
-        while ( idx < arr.length-1 ) {
-            if ( arr[idx].compareTo(arr[idx + 1]) > 0 ) {
-                break;
-            }
-            idx++;
-        }
-        sort(arr, tempArr, idx);
+        sort(arr, tempArr, 0);
     }
 
     private static void sort(Comparable[] arr, Comparable[] tempArr, int mid) {
-        int high = 0;
-        while ( !(high == arr.length-1) ) {
-            high = mid+1;
-            while ( high < arr.length-1 ) {
-                if ( arr[high].compareTo(arr[high + 1]) > 0 ) {
-                    break;
-                }
+        int high = mid+1 ;
+        while ( high < arr.length ) {
+            if ( high == arr.length-1 || arr[high].compareTo(arr[high + 1]) > 0 ) {
+                merge(arr, tempArr, 0, mid, high);
+                mid = high;
+                high = mid +1;
+            }
+            else {
                 high++;
             }
-            merge(arr, tempArr, 0, mid, high);
-            mid = high;
         }
     }
 
--- sortAlgorithm/src/daeHyun/Test.java
+++ sortAlgorithm/src/daeHyun/Test.java
@@ -11,21 +11,20 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         // 시간비교
-       /* for ( int i = 1; i < 1000001; i = i*10) {
-       // for ( int i = 100000; i > 0; i = i/10) {
+       // for ( int i = 1; i < 1000001; i = i*10) {
+        for ( int i = 100000; i > 0; i = i/10) {
             Integer[] arr = RandomArrayCreator.intArr(i);
             long beforeTime = System.nanoTime();
-            BubbleSort.sort(arr);
+            NaturalMergeSort.sort(arr);
             long afterTime = System.nanoTime();
             double time = (double)(afterTime - beforeTime)/1000000;
-            BigDecimal time2 = new BigDecimal(time);
-            System.out.println(i + " time : " + time2);
-        }*/
+            System.out.println(i + " time : " + time + isSorted(arr));
+        }
 
         //정렬확인
-        Integer[] arr = RandomArrayCreator.intArr(1000);
+        /*Integer[] arr = RandomArrayCreator.intArr(1000);
         NaturalMergeSort.sort(arr);
-        System.out.print(isSorted(arr));
+        System.out.print(isSorted(arr));*/
     }
 
     public static boolean isSorted(Comparable[] arr) {
